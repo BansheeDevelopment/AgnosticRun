6f7d6bf16650feb3076d8d2f7f56c771c0271a96
SurceBeats <subcero@gmail.com>
2024-10-02 02:44:24 +0200

    validation, error handler, sanitized, alert if missing + fn type, return type

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-// AgnosticRun 0.0.1 by Banshee
+// AgnosticRun 0.1.2 by Banshee
 // MIT License
 // https://www.banshee.pro
 
 export function agnosticRun(fn) {
-    return function (elementId, ...args) {
-      const element = document.getElementById(elementId);
-      if (element) {
-        if (!element.dataset.checked) {
-          console.log(`AgnosticRun: "${elementId}" exists. Safe to run.`);
-          element.dataset.checked = "true";
-        }
-        return fn(element, ...args);
+  return function (elementId, ...args) {
+    if (typeof elementId !== "string" || !elementId.trim()) {
+      console.error(`AgnosticRun: Invalid elementId "${elementId}". It must be a non-empty string.`);
+      return;
+    }
+    const element = document.getElementById(elementId);
+    if (element) {
+      if (!element.dataset.checked) {
+        console.log(`AgnosticRun: "${elementId}" exists. Safe to run.`);
+        element.dataset.checked = "true";
       }
-    };
-  }
-  
+      return fn(element, ...args);
+    } else {
+      console.warn(`AgnosticRun: Element with ID "${elementId}" does not exist, have you removed it?`);
+    }
+  };
+}
